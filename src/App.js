0597f7ccbc63fc19e65c3b90a75565c945a08788
JessieYG11 <yueguan1110@gmail.com>
2020-07-16 12:19:05 +0200

    :sparkles: Add properties refactored

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React, { useState } from "react";
 import Tweet from "./Tweet";
 
 function App() {
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-    setRed(!isRed);
-  };
+  const [user, setUsers] = useState([
+    { name: "Ed", message: "Hello there" },
+    { name: "John Snow", message: "I am the king of the north!" },
+    { name: "Daenerys Targaryen", message: "Free the slaves!" },
+  ]);
 
   return (
     <div className="app">
-      <h1 className={isRed ? "red" : ""}>Change my color!</h1>
-      <button onClick={increment}>Increment</button>
-      <h1>{count}</h1>
+      {user.map((user) => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
