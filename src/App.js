bce399c0fa751c6c6cb60775e38767d6d5498663
JessieYG11 <yueguan1110@gmail.com>
2020-07-16 12:12:56 +0200

    :sparkles: Add and remove classes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Tweet from "./Tweet";
 
 function App() {
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  };
+
   return (
     <div className="app">
-      <Tweet name="Dev Ed" message="This is a random tweet" />
-      <Tweet name="John Snow" message="Winter is coming" />
-      <Tweet name="Daenerys Targaryen" message="Mother of dragons" />
+      <h1 className={isRed ? "red" : ""}>Change my color!</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
     </div>
   );
 }
